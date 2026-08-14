31c5e4cd531c4eafe3e5248f265bc842dca3c4cc
ColinCampbellCQ <colin.campbell@infleqtion.com>
2026-08-14 17:34:36 -0500

    formatting

--- resource_estimation/__init__.py
+++ resource_estimation/__init__.py
@@ -12,9 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from . import (
+    analysis,
     compile_gateset,
     ftqc,
-    analysis,
 )
 
 __all__ = [
